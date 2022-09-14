973be27e7d5e52e5773abf61c2f668e77f668e7c
Madison-Cox <madison.cox5285@hotmail.com>
2022-09-14 09:37:18 -0500

    added script to html

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+<script type="module" src="index.html"></script>
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
