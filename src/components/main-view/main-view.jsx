ca8e6c36c57b7998ea9cd7d82bdce54fe8526632
Madison-Cox <madison.cox5285@hotmail.com>
2022-09-15 10:17:12 -0500

    import mainview

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className="main-view">
+        <div>Interstellar</div>
+        <div>Hook</div>
+        <div>Goodfellas</div>
+      </div>
+    );
+  }
+}
