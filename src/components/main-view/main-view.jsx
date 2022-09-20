1a89985577c0a2f4e821c6ad2ae8e74d2bf01a1b
Madison-Cox <madison.cox5285@hotmail.com>
2022-09-20 09:07:02 -0500

    installed axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,41 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Interstellar', Description: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.', ImagePath: 'https://i.ytimg.com/vi/uaSYEUugnzE/movieposter_en.jpg' },
-        { _id: 2, Title: 'Hook', Description: 'When Captain James Hook kidnaps his children, an adult Peter Pan must return to Neverland and reclaim his youthful spirit in order to challenge his old enemy.', ImagePath: 'https://i.ytimg.com/vi/BqzG17rxAkw/hqdefault.jpg' },
-        { _id: 3, Title: 'Goodfellas', Description: 'The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.', ImagePath: 'https://i.ytimg.com/vi/3yWyYhF1Ag0/movieposter_en.jpg' },
-      ],
+      movies: [],
       selectedMovie: null
-    };
+    }
+
+    this.componentDidMount(){
+      axios.get('https://moviescoutdb.herokuapp.com/movies')
+        .then(response => {
+          this.setState({
+            movies: response.data
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
+    setSelectedMovie(newSelectedMovie) {
+      this.setState({
+        selectedMovie: newSelectedMovie
+      });
+    }
+
+    render() {
+      const { movies, selectedMovie } = this.state;
+
+
+      if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+      return (
+        <div className="main-view">
+          {selectedMovie
+            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+            : movies.map(movie => (<MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+            ))
+          }
+        </div>
+      );
+    }
   }
-
-  setSelectedMovie(newSelectedMovie) {
-    this.setState({
-      selectedMovie: newSelectedMovie
-    });
-  }
-
-  render() {
-    const { movies, selectedMovie } = this.state;
-
-
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-
-    return (
-      <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-          : movies.map(movie => (<MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
-          ))
-        }
-      </div>
-    );
-  }
-}
 
 //export default MainView;
